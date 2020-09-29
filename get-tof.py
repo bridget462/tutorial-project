31ac1e712a30ef29c43167e6324e321948cf717e
bridget462 <bridges.dev.create@gmail.com>
2020-09-29 22:56:20 +0900

    live graph demo created

--- get-tof.py
+++ get-tof.py
@@ -2,7 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 print('library imported')
 
-data = np.random.rand(100)
+# params
+SAMPLING_NUM = 100
+FRAMES = 10
+INTERVAL_TIME_SEC = 1
 
-plt.plot(data)
-plt.show()
+for i in range(FRAMES):
+    data = np.random.rand(SAMPLING_NUM)
+
+    plt.plot(data)
+
+    if (i == FRAMES):
+        plt.show()
+    else:
+        plt.draw()
+        plt.pause(INTERVAL_TIME_SEC)
+        plt.cla()
